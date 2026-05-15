d66d7a9fa6a0c04192e9e441e254c27478f4df5e
embeddedJedii <ridwan.innov8@gmail.com>
2026-05-15 15:23:20 +0100

    Getting a new folder

--- Smart Incubation system/include/motor.h
+++
@@ -1,9 +0,0 @@
-#ifndef MOTOR_H
-#define MOTOR_H
-
-#include <Arduino.h>
-
-extern volatile uint8_t MOTOR_PIN;
-void IRAM_ATTR driveMotor();
-
-#endif
--- Smart Incubation system/src/motor.cpp
+++
@@ -1,9 +0,0 @@
-#include <Arduino.h>
-
-
-volatile uint8_t MOTOR_PIN = 21;
-
-void IRAM_ATTR driveMotor() {
-  // Code to drive the motor clockwise
-  digitalWrite(MOTOR_PIN, HIGH);
-}
